1eb5fecbfc0839d0ea71073379a253cfcc586228
ShanoBueno <shane.messmer1@gmail.com>
2022-08-15 19:39:21 -0400

    Add add file, delete file, list files functionality

--- src/filesurfer/FileSurferMain.java
+++ src/filesurfer/FileSurferMain.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.util.Scanner; 
 public class FileSurferMain {
 	public static void main(String[] args) {
-		String dirname = "/tmp/user1/java1/bin";
+		String dirname = "/tmp/user/java/bin";
+		File d = new File(dirname);
+		d.mkdir(); 
 		Scanner sc = new Scanner(System.in);
 		int mainOption = 0; 
 		System.out.println("Welcome to File Surfer");
@@ -28,7 +30,7 @@ public class FileSurferMain {
 			System.out.println("====================");
 			System.out.println("1. Add File");
 			System.out.println("2. Delete File");
-			System.out.println("3. Obtain File"); 
+			System.out.println("3. Search For File"); 
 			System.out.println("4. Main Menu");
 			System.out.println("====================");
 			fileMenuOption = sc.nextInt();
@@ -45,12 +47,28 @@ public class FileSurferMain {
 					e.printStackTrace();
 				} 
 				
+			} else if ( fileMenuOption == 2) {
+				System.out.println("What is the name of the file you'd like to delete?");
+				String fileToDelete = sc.next(); 
+				File thisFile = new File(dirname + "/"+fileToDelete); 
+				if (thisFile.exists()) {
+					thisFile.delete();
+					System.out.println("File deleted"); 
+					
+				} else {
+					System.out.println("No File exists by that name"); 
+				}
+ 
+				
 			}
 
 		}
 		}
 		else if (mainOption == 1) {
-			System.out.println("1 selected");
+			String[] files = d.list();
+			for (int i = 0; i < files.length; i++) {
+                System.out.println(files[i]);
+            }
 			
 		} else if (mainOption == 3) {
 			System.out.println("Goodbye!");
